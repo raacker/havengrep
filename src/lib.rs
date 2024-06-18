828193c2d9127b5aad291b264c65b4bc13e2285f
haven <haven@woowahan.com>
2024-06-18 21:05:53 +0900

    add unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,58 @@ use std::error::Error;
 use std::fs;
 
 pub fn grep(input: Input) -> Result<(), Box<dyn Error>> {
-    println!("Searching for {}", input.query.green());
-    println!("From file {}", input.file_path.green());
-
     let file_contents = fs::read_to_string(&input.file_path)?;
 
-    println!("File contents\n{file_contents}");
+    let results = if input.ignore_case {
+        search_case_insensitive(&input.query, &file_contents)
+    } else {
+        search(&input.query, &file_contents)
+    };
+    
+    if results.is_empty() {
+        return Err("No matches found".into())
+    }
+
+    for line in results.iter() {
+        println!("{}", line.green());
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(
+    query: &str,
+    contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub struct Input {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Input {
@@ -26,7 +65,65 @@ impl Input {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = true;
 
-        Ok(Input { query, file_path })
+        Ok(Input {
+            query,
+            file_path,
+            ignore_case
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+
+    #[test]
+    fn no_matches() {
+        let query = "rust-";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert!(
+            search(query, contents).is_empty()
+        );
+
+        assert!(
+            search_case_insensitive(query, contents).is_empty()
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let input = Input::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err.red());
+        eprintln!("Problem parsing arguments: {}", err.red());
         process::exit(1);
     });
 
     if let Err(e) = havengrep::grep(input) {
-        println!("Problem reading the file: {}", e.to_string().red());
+        eprintln!("Problem reading the file: {}", e.to_string().red());
         process::exit(1);
     }
 }
